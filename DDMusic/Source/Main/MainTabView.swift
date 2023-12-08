b3e4d68a19d4d7ed3b7ef591fe43756787d7f71b
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-08 16:44:55 +0900

    Feat : Splash Screen, Main Tab

---
+++ DDMusic/Source/Main/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/8/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
---
+++ DDMusic/Util/Extension/Color.swift
@@ -0,0 +1,8 @@
+//
+//  Color.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/8/23.
+//
+
+import Foundation
