e590b21ea34568b88573ac4092dd57f90c13eddf
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-08 16:45:13 +0900

    Fix : Extension Color

--- DDMusic/Util/Extension/Color.swift
+++ DDMusic/Util/Extension/Color.swift
@@ -5,4 +5,23 @@
 //  Created by 남경민 on 12/8/23.
 //
 
-import Foundation
+import SwiftUI
+ 
+extension Color {
+  init(hex: String) {
+    let scanner = Scanner(string: hex)
+    _ = scanner.scanString("#")
+    
+    var rgb: UInt64 = 0
+    scanner.scanHexInt64(&rgb)
+    
+    let r = Double((rgb >> 16) & 0xFF) / 255.0
+    let g = Double((rgb >>  8) & 0xFF) / 255.0
+    let b = Double((rgb >>  0) & 0xFF) / 255.0
+    self.init(red: r, green: g, blue: b)
+  }
+}
+extension Color {
+    static let primary1 = Color(hex: "31C690")
+}
+
