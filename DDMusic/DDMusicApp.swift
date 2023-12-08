c8137ea069463bf191dbcf15f617b9270bd41237
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-08 15:36:19 +0900

    Initial Commit

---
+++ DDMusic/DDMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDMusicApp.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DDMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreen()
+        }
+    }
+}
---
+++ DDMusic/Splash/SplashScreen.swift
@@ -0,0 +1,37 @@
+//
+//  SplashScreen.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/8/23.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State private var isActive = false
+    var body: some View {
+        if isActive {
+
+        } else {
+            ZStack {
+                Image("splash_bg")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                    .ignoresSafeArea()
+                Image("illust_splash")
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                   
+                }
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
