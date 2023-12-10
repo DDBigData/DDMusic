79ac4046393fe0b19487c7ef31f95a8a41722758
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-10 19:24:48 +0900

    Feat : Home View, Playlist View

---
+++ DDMusic/Source/Main/Playlist/PlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistView.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/10/23.
+//
+
+import SwiftUI
+
+struct PlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaylistView()
+}
