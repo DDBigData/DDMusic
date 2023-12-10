1a9e9f68cfc95bb596ac578ddbc39cf68a15ff53
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-10 21:03:20 +0900

    Fix : Music View

--- DDMusic/Source/Main/Music/MusicView.swift
+++ DDMusic/Source/Main/Music/MusicView.swift
@@ -21,8 +21,75 @@ struct MusicView: View {
                 endPoint: UnitPoint(x: 1, y: 1)
             )
             VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 45, height: 24)
+                            .background(Color(red: 0.33, green: 0.18, blue: 0.38))
+                            .cornerRadius(28)
+                        Text("All")
+                            .font(.system(size: 18,weight: .medium))
+                            .foregroundColor(.white)
+                            .frame(width: 36, alignment: .center)
+                    }
+                    Spacer()
+                }
+                .padding(.bottom,20)
+                VStack(spacing:16) {
+                    HStack(spacing:16) {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 69, height: 62)
+                            .background(
+                                //Image("PATH_TO_IMAGE")
+                                //.resizable()
+                                //.aspectRatio(contentMode: .fill)
+                                Color.white
+                                    .frame(width: 69, height: 62)
+                                    .clipped()
+                            )
+                            .cornerRadius(5)
+                        VStack(alignment: .leading,spacing:5) {
+                            Text("Those Eyes")
+                                .font(.system(size: 15,weight: .semibold))
+                                .foregroundColor(.white)
+                            Text("New west")
+                                .font(.system(size: 13,weight: .light))
+                                .foregroundColor(Color(red: 0.85, green: 0.85, blue: 0.85))
+                        }
+                        Spacer()
+                        Image("icon_heart")
+                    }
+                    HStack(spacing:16) {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 69, height: 62)
+                            .background(
+                                //Image("PATH_TO_IMAGE")
+                                //.resizable()
+                                //.aspectRatio(contentMode: .fill)
+                                Color.white
+                                    .frame(width: 69, height: 62)
+                                    .clipped()
+                            )
+                            .cornerRadius(5)
+                        VStack(alignment: .leading,spacing:5) {
+                            Text("Those Eyes")
+                                .font(.system(size: 15,weight: .semibold))
+                                .foregroundColor(.white)
+                            Text("New west")
+                                .font(.system(size: 13,weight: .light))
+                                .foregroundColor(Color(red: 0.85, green: 0.85, blue: 0.85))
+                        }
+                        Spacer()
+                        Image("icon_heart_filled")
+                    }
+                }
+                Spacer()
             }
+            .padding(.top,100)
+            .padding(.horizontal,20)
         }
         .frame(maxWidth: .infinity,maxHeight: .infinity)
         .ignoresSafeArea()
