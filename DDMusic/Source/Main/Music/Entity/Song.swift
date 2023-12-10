85cc90f7291db969437590883c0cb5f59fbb5f7a
kyoungmin nam <58581608+devzoe@users.noreply.github.com>
2023-12-10 23:14:08 +0900

    Feat : Song Entity, Music View Model

---
+++ DDMusic/Source/Main/Music/Entity/Song.swift
@@ -0,0 +1,8 @@
+//
+//  Music.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/10/23.
+//
+
+import Foundation
---
+++ DDMusic/Source/Main/Music/MusicViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MusicViewModel.swift
+//  DDMusic
+//
+//  Created by 남경민 on 12/10/23.
+//
+
+import Foundation
